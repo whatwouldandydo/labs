ce686f8b1e8fe80853093db8e2a0cd4a1139ba7a
Andy Bosco <andysodo@hotmail.com>
2021-06-10 00:30:50 -0700

    Format code to pep8 style

--- Systems/tcp_tuning.py
+++ Systems/tcp_tuning.py
@@ -4,24 +4,23 @@ Summary: The tcp_tuning.py provides a recommended system window size settings
 to achieve the maximum bandwidth from your network.
 """
 
+
 def tcp_tuning():
     warning_screen = """
-    ==================================================================================
-    | WARNING: This works only on Linux, UNIX, and AIX systems. Run command          |
-    |                                                                                |
-    | sudo sysctl -a | egrep "[wr]mem" | egrep "core|tcp"                            |
-    |                                                                                |
-    | so you have a backup to restore the system to its original state.              |
-    ==================================================================================
+    =========================================================================
+    | WARNING: This works only on Linux, UNIX, and AIX systems. Run command |
+    |                                                                       |
+    | sudo sysctl -a | egrep "[wr]mem" | egrep "core|tcp"                   |
+    |                                                                       |
+    | so you have a backup to restore the system to its original state.     |
+    =========================================================================
     """
     print(warning_screen)
-    
     """ Provide display options and user to pick one. """
     opt1 = "1. I know my network speed."
     opt2 = "2. I know my system window size."
 
     print(f"Please selection option 1 or 2: \n{opt1}\n{opt2}\n")
-    
     try:
         user_input = int(input("Option: "))
 
@@ -45,7 +44,7 @@ def tcp_tuning():
                 print(f'sudo sysctl -w net.core.wmem_max={cal_win * 4}')
                 print("-" * 100)
             except ValueError:
-                print("Please enter a whole number with no comma or period.")
+                print("Please enter a whole number with no comma or period.\n")
         elif user_input == 2:
             """
             Calculate network speed base on system window size and latency.
@@ -57,17 +56,20 @@ def tcp_tuning():
                 cal_speed_bit = int(window * 8 / net_delay / 1000)
                 # cal_speed_byte = int(window / net_delay / 1000)
                 print()
-                print(f'"{cal_speed_bit} Mbits/sec" is the maximum network speed for Window Size "{window}" bytes with "{net_delay}" milliseconds latency.')
+                if cal_speed_bit > 0:
+                    print(f'"{cal_speed_bit} Mbits/sec" is the maximum network speed for Window Size "{window}" bytes with "{net_delay}" milliseconds latency.\n')
+                else:
+                    print(f'Something is wrong. Your network speed is below 1 Mbit/sec.\n')
             except ValueError:
-                print("Please enter a whole number with no comma or period.")
+                print("Please enter a whole number with no comma or period.\n")
         else:
-            print("Exist Program ...")
+            print("Exist Program ...\n")
             exit
 
     except ValueError:
-            """ Error handling to catch non-integer 1 or 2 """
-            print("Please enter number 1 or 2.")
+        """ Error handling to catch non-integer 1 or 2 """
+        print("Please enter number 1 or 2.\n")
 
 
 if __name__ == "__main__":
-    test = tcp_tuning()
+    run_tcp = tcp_tuning()
