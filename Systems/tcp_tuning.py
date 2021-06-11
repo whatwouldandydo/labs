97876d41ab094352f1c27649f97d7d3d4f582613
Andy Bosco <andysodo@hotmail.com>
2021-06-10 21:38:02 -0700

    Add error handling to catch ZeroDivision

--- Systems/tcp_tuning.py
+++ Systems/tcp_tuning.py
@@ -4,6 +4,7 @@ Date: 2021-06-09
 Summary: The tcp_tuning.py provides a recommended system window size settings
 to achieve the maximum bandwidth from your network.
 """
+import sys
 
 
 def tcp_tuning():
@@ -20,8 +21,9 @@ def tcp_tuning():
     """ Provide display options and user to pick one. """
     opt1 = "1. I know my network speed."
     opt2 = "2. I know my system window size."
+    opt3 = "3. Exit"
 
-    print(f"Please selection option 1 or 2: \n{opt1}\n{opt2}\n")
+    print(f"Please selection option 1 or 2: \n{opt1}\n{opt2}\n{opt3}\n")
     try:
         user_input = int(input("Option: "))
 
@@ -35,7 +37,7 @@ def tcp_tuning():
                 net_delay = int(input("What is your network latency in milliseconds: "))
                 cal_win = int(net_speed * 10**6 / 8 * net_delay / 1000)
                 print()
-                print(f'"{cal_win}" Bytes is the Window Size for the network speed of "{net_speed} Mbits/sec" and "{net_delay}" milliseconds latency.')
+                print(f'"{cal_win}" Bytes is the Window Size for the network speed of "{net_speed} Mbits/sec" with "{net_delay}" milliseconds latency.\n')
                 print("-" * 100)
                 print("Recommended Commands to Change Your System Settings:")
                 print("<Unchange> means do not modify this value.\n")
@@ -45,7 +47,7 @@ def tcp_tuning():
                 print(f'sudo sysctl -w net.core.wmem_max={cal_win * 4}')
                 print("-" * 100)
             except ValueError:
-                print("Please enter a whole number with no comma or period.\n")
+                print("\nPlease enter a whole number with no comma or period.\n")
         elif user_input == 2:
             """
             Calculate network speed base on system window size and latency.
@@ -54,18 +56,21 @@ def tcp_tuning():
             try:
                 window = int(input("What is your system window size: "))
                 net_delay = int(input("What is your network latency in milliseconds: "))
-                cal_speed_bit = int(window * 8 / net_delay / 1000)
-                # cal_speed_byte = int(window / net_delay / 1000)
-                print()
-                if cal_speed_bit > 0:
-                    print(f'"{cal_speed_bit} Mbits/sec" is the maximum network speed for Window Size "{window}" bytes with "{net_delay}" milliseconds latency.\n')
-                else:
-                    print(f'Something is wrong. Your network speed is below 1 Mbit/sec.\n')
+                try:
+                    cal_speed_bit = int(window * 8 / net_delay / 1000)
+                    # cal_speed_byte = int(window / net_delay / 1000)
+                    print()
+                    if cal_speed_bit > 0:
+                        print(f'"{cal_speed_bit} Mbits/sec" is the maximum network speed for Window Size "{window}" bytes with "{net_delay}" milliseconds latency.\n')
+                    else:
+                        print(f'Something is wrong. Your network speed is below 1 Mbit/sec.\n')
+                except ZeroDivisionError:
+                    print(f"\nThe network latency cannot be 0. Please try again...\n")
             except ValueError:
-                print("Please enter a whole number with no comma or period.\n")
+                print("\nPlease enter a whole number with no comma or period.\n")
         else:
             print("Exist Program ...\n")
-            exit
+            sys.exit()
 
     except ValueError:
         """ Error handling to catch non-integer 1 or 2 """
