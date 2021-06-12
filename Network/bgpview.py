cd74b402ad01b04314d5cc186c874a1ea9db7a7c
Andy Bosco <andysodo@hotmail.com>
2021-06-12 12:32:56 -0700

    Add *arg to allow more AS number in the function.

--- Network/bgpview.py
+++ Network/bgpview.py
@@ -1,4 +1,5 @@
 """
+Author: www.linkedin.com/in/whatwouldandydo
 Date: 2021-06-10
 Summary: Use BGPView API to perform nslookup for BGP ASN, Prefixes, Peers,
 Upstreams, Downstream and IP.
@@ -35,56 +36,54 @@ class BGPView:
         # Replace ix_id with integer.
         self.ix_api = "https://api.bgpview.io/ix/ix_id"
 
-    def get_asn(self, as_number):
+    def get_asn(self, *as_number):
         """
-        Get ASN information such as country, name, looking glass,
-        bandwidth, allocation status.
+        Allow as many AS number as input and find ASN
+        information such as, name, country, looking glass,
+        bandwidth, allocation status, and more.
         """
-        try:
-            # Check if as_number is integer
-            # as_number = int(input("AS Number: "))
-            as_number = 59687
-            asn_api = self.asn_api.replace("as_number", str(as_number))
-            web_request = requests.get(f"{asn_api}", verify=False)
-            print(web_request) # <Response [200]>
+        for number in as_number:
+            try:
+                asn_api = self.asn_api.replace("as_number", str(number))
+                print(asn_api)
+                web_request = requests.get(f"{asn_api}", verify=False)
+                print(web_request) # <Response [200]>
 
-            # Retry the API 3 times with 3 second wait when fail
-            query_count = 0
-            while query_count != 3:
-                query_count += 1
-                time.sleep(3)
-                if web_request.status_code == 200:
-                    meta = web_request.json()
-                    data = meta["data"]
-                    status = meta["status"]
-                    asn = data["asn"]
-                    country_code = data["country_code"]
-                    description_short = data["description_short"]
-                    looking_glass = data["looking_glass"]
-                    rir_name = data["rir_allocation"]["rir_name"]
-                    allocation_status = data["rir_allocation"]["allocation_status"]
-                    date_allocated = data["rir_allocation"]["date_allocated"]
-                    traffic_estimation = data["traffic_estimation"]
-                    traffic_ratio = data["traffic_ratio"]
-                    website = data["website"]
-                    assignment_status = data["iana_assignment"]["assignment_status"]
-                    date_updated = data["date_updated"]
-                    print(date_updated)
-                    break
-            else:
-                print(f"Query Fail===> {web_request}")
+                # When API request fails, retry it 3 times with 3 seconds wait
+                query_count = 0
+                while query_count != 3:
+                    query_count += 1
+                    time.sleep(3)
+                    if web_request.status_code == 200:
+                        meta = web_request.json()
+                        data = meta["data"]
+                        status = meta["status"]
+                        asn = data["asn"]
+                        country_code = data["country_code"]
+                        description_short = data["description_short"]
+                        looking_glass = data["looking_glass"]
+                        rir_name = data["rir_allocation"]["rir_name"]
+                        allocation_status = data["rir_allocation"]["allocation_status"]
+                        date_allocated = data["rir_allocation"]["date_allocated"]
+                        traffic_estimation = data["traffic_estimation"]
+                        traffic_ratio = data["traffic_ratio"]
+                        website = data["website"]
+                        assignment_status = data["iana_assignment"]["assignment_status"]
+                        date_updated = data["date_updated"]
+                        print(f"{description_short}\n")
+                        break
+                else:
+                    print(f"ERROR {web_request}: Try to access {asn_api} 3 times but fail.\n")
 
-        except:
-            print("Invalid AS Number")
+            except:
+                print(f"ERROR: {number} is NOT a valid AS number.\n")
 
 """
-1. Use kwargs to allow multiple arguments
 2. Retrieve indiviual attribute such as asn, rir_name, looking_glass
 """
 
 t1 = BGPView()
-t1.get_asn(1234)
+t1.get_asn(1,100,200,300,"dfsd",555.55,"666.abc","xyz.987")
 
 t2 = BGPView()
-# t2.get_asn("dsd")
-
+t2.get_asn(65124)
