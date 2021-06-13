537236492d985c77821929bbdfa70cccba4dc221
Andy Bosco <andysodo@hotmail.com>
2021-06-12 20:08:53 -0700

    Convert get_asn variables as instance variables accessing from outside class BGPView. Still cannot retreive all instances from get_asn(1000,2000,3000,4000) It only retreives variable instances for the last 4000

--- Network/bgpview.py
+++ Network/bgpview.py
@@ -15,27 +15,72 @@ from urllib3.exceptions import InsecureRequestWarning
 """ Disable SSL warning self-sign certificate """
 requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
 
+# main_url = "https://api.bgpview.io/"
+# asn_api = "https://api.bgpview.io/asn/as_number"
+# asn_prefixes_api = "https://api.bgpview.io/asn/as_number/prefixes"
+# asn_peers_api = "https://api.bgpview.io/asn/as_number/peers"
+# asn_upstreams_api = "https://api.bgpview.io/asn/as_number/upstreams"
+# asn_downstreams_api = "https://api.bgpview.io/asn/as_number/downstreams"
+# asn_ixs_api = "https://api.bgpview.io/asn/as_number/ixs"
 
 class BGPView:
+    # main_url = "https://api.bgpview.io/"
+    # asn_api = "https://api.bgpview.io/asn/as_number"
+    # asn_prefixes_api = "https://api.bgpview.io/asn/as_number/prefixes"
+    # asn_peers_api = "https://api.bgpview.io/asn/as_number/peers"
+    # asn_upstreams_api = "https://api.bgpview.io/asn/as_number/upstreams"
+    # asn_downstreams_api = "https://api.bgpview.io/asn/as_number/downstreams"
+    # asn_ixs_api = "https://api.bgpview.io/asn/as_number/ixs"
+
+    # global asn_api
+    # asn_api = "https://api.bgpview.io/asn/as_number"
+
     def __init__(self):
         """ Endpoint APIs """
-        # Replace as_number with integer.
-        self.main_url = "https://api.bgpview.io/"
+        # # Replace as_number with integer.
+        # main_url = "https://api.bgpview.io/"
         self.asn_api = "https://api.bgpview.io/asn/as_number"
-        self.asn_prefixes_api = "https://api.bgpview.io/asn/as_number/prefixes"
-        self.asn_peers_api = "https://api.bgpview.io/asn/as_number/peers"
-        self.asn_upstreams_api = "https://api.bgpview.io/asn/as_number/upstreams"
-        self.asn_downstreams_api = "https://api.bgpview.io/asn/as_number/downstreams"
-        self.asn_ixs_api = "https://api.bgpview.io/asn/as_number/ixs"
+        # asn_prefixes_api = "https://api.bgpview.io/asn/as_number/prefixes"
+        # asn_peers_api = "https://api.bgpview.io/asn/as_number/peers"
+        # asn_upstreams_api = "https://api.bgpview.io/asn/as_number/upstreams"
+        # asn_downstreams_api = "https://api.bgpview.io/asn/as_number/downstreams"
+        # asn_ixs_api = "https://api.bgpview.io/asn/as_number/ixs"
 
-        # Replace ip_address/cidr with network/mask (/24).
-        self.prefix_api = "https://api.bgpview.io/prefix/ip_address/cidr"
+        # # Replace ip_address/cidr with network/mask (/24).
+        # prefix_api = "https://api.bgpview.io/prefix/ip_address/cidr"
 
-        # Replace ip_address with individual IP address.
-        self.ip_api = "https://api.bgpview.io/ip/ip_address"
+        # # Replace ip_address with individual IP address.
+        # ip_api = "https://api.bgpview.io/ip/ip_address"
 
-        # Replace ix_id with integer.
-        self.ix_api = "https://api.bgpview.io/ix/ix_id"
+        # # Replace ix_id with integer.
+        # ix_api = "https://api.bgpview.io/ix/ix_id"
+
+        # ASN health status
+        self.status = None
+        # ASN number
+        self.asn = None
+        # ASN origin country code
+        self.country_code = None
+        # ASN name
+        self.description_short = None
+        # ASN looking glass website 
+        self.looking_glass = None
+        # ASN RIR name
+        self.rir_name = None
+        # ASN regional (IRIN) allocation status (assigned, unassigned)
+        self.allocation_status = None
+        # ASN creation date
+        self.date_allocated = None
+        # ASN traffic direction
+        self.traffic_estimation = None
+        # ASN traffic ratio
+        self.traffic_ratio = None
+        # ASN company website
+        self.website = None
+        # ASN IANA allocation status (assigned, unassigned)
+        self.assignment_status = None
+        # ASN last updated date
+        self.date_updated = None
 
     def get_asn(self, *as_number):
         """
@@ -43,12 +88,15 @@ class BGPView:
         information such as, name, country, looking glass,
         bandwidth, allocation status, and more.
         """
+        # Replace as_number with numberical value.
+        # asn_api = "https://api.bgpview.io/asn/as_number"
         for number in as_number:
             try:
+                # asn_api = asn_api.replace("as_number", str(number))
                 asn_api = self.asn_api.replace("as_number", str(number))
                 print(asn_api)
                 web_request = requests.get(f"{asn_api}", verify=False)
-                print(web_request) # <Response [200]>
+                # print(web_request) # <Response [200]>
 
                 # When API request fails, retry it 3 times with 3 seconds wait
                 query_count = 0
@@ -57,40 +105,42 @@ class BGPView:
                     time.sleep(3)
 
                     if web_request.status_code == 200:
+                        # time.sleep(1)
                         meta = web_request.json()
+                        # print(meta)
                         data = meta["data"]
                         # ASN health status
-                        status = meta["status"]
+                        self.status = meta["status"]
                         # ASN number
-                        asn = data["asn"]
+                        self.asn = data["asn"]
                         # ASN origin country code
-                        country_code = data["country_code"]
+                        self.country_code = data["country_code"]
                         # ASN name
-                        description_short = data["description_short"]
+                        self.description_short = data["description_short"]
                         # ASN looking glass website 
-                        looking_glass = data["looking_glass"]
+                        self.looking_glass = data["looking_glass"]
                         # ASN RIR name
-                        rir_name = data["rir_allocation"]["rir_name"]
+                        self.rir_name = data["rir_allocation"]["rir_name"]
                         # ASN regional (IRIN) allocation status (assigned, unassigned)
-                        allocation_status = data["rir_allocation"]["allocation_status"]
+                        self.allocation_status = data["rir_allocation"]["allocation_status"]
                         # ASN creation date
-                        date_allocated = data["rir_allocation"]["date_allocated"]
+                        self.date_allocated = data["rir_allocation"]["date_allocated"]
                         # ASN traffic direction
-                        traffic_estimation = data["traffic_estimation"]
+                        self.traffic_estimation = data["traffic_estimation"]
                         # ASN traffic ratio
-                        traffic_ratio = data["traffic_ratio"]
+                        self.traffic_ratio = data["traffic_ratio"]
                         # ASN company website
-                        website = data["website"]
+                        self.website = data["website"]
                         # ASN IANA allocation status (assigned, unassigned)
-                        assignment_status = data["iana_assignment"]["assignment_status"]
+                        self.assignment_status = data["iana_assignment"]["assignment_status"]
                         # ASN last updated date
-                        date_updated = data["date_updated"]
-                        print(f"{description_short}\n")
+                        self.date_updated = data["date_updated"]
+                        # print(f"{description_short}\n")
                         break
                 else:
                     print(f"ERROR {web_request}: Try to access {asn_api} 3 times but fail.\n")
 
-            except TypeError:
+            except KeyError:
                 print(f"ERROR: {number} is NOT a valid AS number.\n")
 
             except:
@@ -99,11 +149,20 @@ class BGPView:
                 print()
 
 """
-2. Retrieve indiviual attribute such as asn, rir_name, looking_glass
+2. Access attribute such as country_code, description_short, from t1.get_asn
 """
 
 t1 = BGPView()
-t1.get_asn(1,100,200,300,"dfsd",555.55,"666.abc","xyz.987")
+# t1.get_asn(1,100,"dfsd",555.55,"666.abc","xyz.987")
+t1.get_asn(1000,2000,3000,4000)
+print(t1.asn, t1.description_short, t1.status)
 
 t2 = BGPView()
-t2.get_asn(65124)
+t2.get_asn(111)
+print(t2.asn, t2.description_short, t2.status)
+t2.get_asn(222)
+print(t2.asn, t2.description_short, t2.status)
+
+# t3 = BGPView()
+# print(t3.asn)
+# print(t3.status)
